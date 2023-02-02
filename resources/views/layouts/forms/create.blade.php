7e3563bd36b65438e1e694f4a35227b26768fd7d
Francesc Bagué Martí (cep) <fbaguem2021@cepnet.net>
2023-02-02 13:26:49 +0100

    Cambio en la vista 'create'

--- resources/views/layouts/forms/create.blade.php
+++ resources/views/layouts/forms/create.blade.php
@@ -1,6 +1,1 @@
 @section('title', 'Pokedex | Crear Pokemon')
-<div class="card mt-2">
-    <div class="card-header bg-secondary text-white">
-        <img src="{{ asset('') }}" alt="">
-    </div>
-</div>
